c09d953de3c69d70ba106f73e4b7f4492f21eb13
bart-bialk <bartbialk@gmail.com>
2025-10-28 11:37:06 +0100

    zadanie 2 dodane

--- src/pipelines/data_preparation/pipeline.py
+++ src/pipelines/data_preparation/pipeline.py
@@ -3,8 +3,43 @@ This is a boilerplate pipeline 'data_preparation'
 generated using Kedro 1.0.0
 """
 
-from kedro.pipeline import Node, Pipeline  # noqa
+from kedro.pipeline import Node, Pipeline, node, pipeline
+from .nodes import *
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([])
+    return Pipeline(
+        [
+            node(
+                func=load_raw,
+                inputs=None,
+                outputs='raw_data',
+                name="load_raw_data"
+        ),
+            node(
+                func=basic_clean,
+                inputs='raw_data',
+                outputs='clean_data',
+                name="basic_clean"
+            ),
+            node(
+                func=train_test_split,
+                inputs='clean_data',
+                outputs=['X_train', 'X_test', 'y_train', 'y_test'],
+                name="train_test_split"
+            ),
+            node(
+                func=train_baseline,
+                inputs='train_test_split',
+                outputs='model_baseline',
+                name="train_baseline"
+            ),
+            node(
+                func=evaluate,
+                inputs='model_baseline',
+                outputs='metrics_baseline',
+                name="evaluate"
+            )
+        ]
+
+    )
